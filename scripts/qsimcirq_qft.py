c50869565049a4b7be669aea2cac55ec716efc9e
Daniel Strano <stranoj@gmail.com>
2023-03-22 11:18:05 -0400

    qsimcirq needs to run in 'appliance'

---
+++ scripts/qsimcirq_qft.py
@@ -0,0 +1,48 @@
+import time
+import random
+import math
+import cirq
+import qsimcirq
+import cupy
+
+
+low = 1
+high = 27
+samples = 10
+
+def cuquantum_qft(q):
+    qreg = list(q)
+    for j in range(len(qreg)):
+        for k in range(j):
+            yield (cirq.CZ ** (2**(j-k)))(qreg[j], qreg[k])
+        yield cirq.H(qreg[j])
+
+    start = 0
+    end = len(qreg) - 1
+    while (start < end):
+        yield cirq.SWAP(qreg[start], qreg[end])
+        start += 1
+        end -= 1
+
+    yield cirq.measure(*qreg)
+
+def bench_cuquantum(n):
+    qubits = cirq.LineQubit.range(n)
+    qft = cirq.Circuit(cuquantum_qft(qubits))
+    simulator = qsimcirq.QSimSimulator(qsimcirq.QSimOptions(gpu_mode=1))
+
+    start = time.perf_counter()
+    simulator.run(qft, repetitions=1)
+    return time.perf_counter() - start
+
+cuquantum_results = {}
+for n in range(low, high + 1):
+    width_results = []
+         
+    # Run the benchmarks
+    for i in range(samples):
+        width_results.append(bench_cuquantum(n))
+
+    cuquantum_results[n] = sum(width_results) / samples
+
+print(cuquantum_results)
